c6a9000d027ff26870767cde8144ff0f41b62f68
YAro <carlend94@ukr.net>
2020-09-10 14:54:13 +0300

    added one way binding

--- src/app/binding/binding.component.ts
+++ src/app/binding/binding.component.ts
@@ -6,14 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./binding.component.scss']
 })
 export class BindingComponent implements OnInit {
-
-  img = 'https://www.filepicker.io/api/file/SVFQZQAyRpqJ31f6LNGe'
+  img = 'https://www.filepicker.io/api/file/SVFQZQAyRpqJ31f6LNGe';
+  inputValue: any;
 
   constructor() {
     setTimeout(() => this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png', 5000)
   }
 
   ngOnInit(): void {
+  }
+
+  methodForCall(event?) {
+    this.inputValue = event.target.value
+    console.log('event', event)
   }
 
 }
