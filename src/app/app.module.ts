a1718f9f12a0131f9f32feab4f0d457a1788574a
YAro <carlend94@ukr.net>
2020-09-10 13:01:48 +0300

    added skeleton

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {PostComponent} from './post/post.component';
+import { Post2Component } from './post2/post2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponent,
+    Post2Component
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
