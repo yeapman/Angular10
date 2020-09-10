32f7ed24400638d5826f17ba7089dd550966ec17
YAro <carlend94@ukr.net>
2020-09-10 17:04:39 +0300

    added structure directive *ngFor

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -9,6 +9,15 @@ export class DirectivesComponent implements OnInit {
   backgroundToggle: boolean = true;
   toggleForStructureDirective: boolean = true;
   switch: boolean;
+  arr = [1,2,3,4,5]
+
+  hardArrWithObjects = [
+    {title: 'Post 1', author: 'Vladilen', comments: [
+        {name: 'Yarik', text: 'lorem 1'},
+        {name: 'Vasya', text: 'hardVasya'}
+      ]}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
