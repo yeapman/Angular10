aa6617bb6805df55a62f8bf131a737a54b917109
YAro <carlend94@ukr.net>
2020-09-10 16:11:36 +0300

    example two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { InterpolationComponent } from './interpolation/interpolation.component';
 import { BindingComponent } from './binding/binding.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BindingComponent } from './binding/binding.component';
     BindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/binding/binding.component.ts
+++ src/app/binding/binding.component.ts
@@ -6,22 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./binding.component.scss']
 })
 export class BindingComponent implements OnInit {
-  img = 'https://www.filepicker.io/api/file/SVFQZQAyRpqJ31f6LNGe';
-  inputValue: any;
+  text = "Initial text";
 
   constructor() {
-    setTimeout(() => this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png', 5000)
   }
 
   ngOnInit(): void {
   }
 
-  methodForCall(event?) {
-    this.inputValue = event.target.value
-  }
-
-  onInput(event?) {
-    console.log(event)
+  onInput(event: any) {
+    this.text = event.target.value
   }
 
 }
