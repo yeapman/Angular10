452af25965685036c5cd9bf86a8a4b93feb632a1
YAro <carlend94@ukr.net>
2020-09-13 18:06:55 +0300

    added more good variant via HostBinding style.color

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
@@ -6,6 +6,10 @@ import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/co
 export class StyleDirective {
   @Input('appStyle') color: string = 'blue';
   @Input() fontWeight: string = 'normal';
+
+  @HostBinding("style.color") elColor = null;
+
+
   constructor(private el: ElementRef, private r: Renderer2) {
     el.nativeElement.style.color = 'red'; // bad use, not optimise
 
@@ -17,11 +21,11 @@ export class StyleDirective {
   }
 
   @HostListener('mouseenter') onMyEnter() {
-    this.r.setStyle(this.el.nativeElement, 'color', this.color);
+    this.elColor = this.color;
   }
 
   @HostListener('mouseleave') onLeave() {
-    this.r.setStyle(this.el.nativeElement, 'color', 'green')
+   this.elColor = null
   }
 
 
