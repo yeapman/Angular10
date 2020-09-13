dc68f8f4f9043d5127fc4274d6d470a5b8fc5d8c
YAro <carlend94@ukr.net>
2020-09-13 17:35:32 +0300

    added Renderer2 for change nativeElement styles

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,8 +1,14 @@
-import {Directive} from '@angular/core';
+import {Directive, ElementRef, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
+
+  constructor(private el: ElementRef, private r: Renderer2) {
+    el.nativeElement.style.color = 'red'; // bad use, not optimise
+
+    this.r.setStyle(this.el.nativeElement, 'color', 'blue') // should use via Renderer2, it is optimise
+  }
 
 }
