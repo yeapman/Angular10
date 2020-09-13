02acff4b7ab677553dbc0fcdb1a65692fcec4ed1
YAro <carlend94@ukr.net>
2020-09-13 15:03:37 +0300

    added component angular form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { InterpolationComponent } from './interpolation/interpolation.component'
 import { BindingComponent } from './binding/binding.component';
 import {FormsModule} from '@angular/forms';
 import {DirectivesComponent} from './directives/directives.component';
+import { JsComponent } from './js/js.component';
+import { ComponentFormComponent } from './component-form/component-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InterpolationComponent,
     BindingComponent,
-    DirectivesComponent
+    DirectivesComponent,
+    JsComponent,
+    ComponentFormComponent
   ],
   imports: [
     BrowserModule,
